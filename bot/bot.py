69ab813f325d34a7b0909cf20c07fb59275f54db
dgc1980 <hunter0@gmail.com>
2021-11-09 23:12:47 +1100

    fixed

--- bot/bot.py
+++ bot/bot.py
@@ -137,9 +137,9 @@ def run_schedule():
         expiremode=0
         new_flair = "";
         if "VANAF MAANDAG / KOOPZEGELS UIT" in old_flair.upper():
-          new_flair = "KORTING LOOPT / KOOPZEGELS AAN"
+          new_flair = "9cce08ea-3080-11ea-bfee-0e3b5579f8e9"
           flair_template = "efabb9c6-b690-11eb-93a2-0e8ba0a235d9"
-        if "VANAF MAANDAG / KOOPZEGELS AAN" in old_flair.upper():
+        if "KORTING LOOPT / KOOPZEGELS AAN" in old_flair.upper():
           new_flair = "KORTING LOOPT / KOOPZEGELS UIT"
           flair_template = "c0a2c902-d261-11eb-b5d5-0e7ad168c343"
 
